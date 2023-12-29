4a7e485e2fdb374155d9ac20dfb825ef54d8b858
ewerlloncbn <ewerllon_cristian@hotmail.com>
2023-12-29 16:57:40 -0400

    :wrench: config - configuration bare workflow with expo prebuild command line and generated android and ios files in config app

--- App.tsx
+++ App.tsx
@@ -1,31 +1,26 @@
-import { StatusBar } from 'react-native'
-import { NativeBaseProvider } from 'native-base'
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold
-} from '@expo-google-fonts/roboto'
+import { OneSignal } from 'react-native-onesignal'
+import { Platform } from 'react-native'
 
-import { Routes } from './src/routes'
+export default function onesignalInitialize() {
+  const {
+    EXPO_PUBLIC_ONE_SIGNAL_APP_ID_ANDROID,
+    EXPO_PUBLIC_ONE_SIGNAL_APP_ID_IOS
+  } = process.env
 
-import { THEME } from './src/theme'
-import { Loading } from './src/components/Loading'
+  const oneSignalAppId =
+    Platform.OS === 'ios'
+      ? EXPO_PUBLIC_ONE_SIGNAL_APP_ID_IOS
+      : EXPO_PUBLIC_ONE_SIGNAL_APP_ID_ANDROID
 
-import { CartContextProvider } from './src/contexts/CartContext'
+  if (oneSignalAppId) {
+    OneSignal.initialize(oneSignalAppId)
+  }
 
-export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
-
-  return (
-    <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <CartContextProvider>
-        {fontsLoaded ? <Routes /> : <Loading />}
-      </CartContextProvider>
-    </NativeBaseProvider>
-  )
+  if (Platform.OS === 'ios') {
+    OneSignal.Notifications.canRequestPermission().then(response => {
+      if (response) {
+        OneSignal.Notifications.requestPermission(true)
+      }
+    })
+  }
 }
